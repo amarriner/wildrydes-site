266be22cac742a1817e0cebef9076f274ab10114
Aaron Marriner <amarriner@amarriner.com>
2019-08-12 15:07:48 -0400

    Updating pool ids

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_gQ24vQTYG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '475m56ct81lvbajmltgoh6hb1i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
